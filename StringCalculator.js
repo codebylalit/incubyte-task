068e2b58e7d554256b3344e948dbb0193c996e91
codebylalit <lalitkumarnamdev4645@gmail.com>
2024-07-19 16:06:03 +0530

    pass new test

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,6 +2,9 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
+
+  const nums = numbers.split(",").map(Number);
+  return nums.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
